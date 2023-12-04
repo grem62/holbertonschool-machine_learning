c99689a81383ece9cd7da406886eff9add915aa4
grem62 <gremontmatheo22@gmail.com>
2023-12-04 16:59:49 +0100

    concatenate array

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+concatenate array
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """_summary_
+
+    Args:
+        arr1 (_type_): _description_
+        arr2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    new_array = [(arr1) + (arr2)]
+    return new_array
