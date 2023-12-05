8c1ff32af996f43f56aaced3657dd86601c1e4c1
grem62 <gremontmatheo22@gmail.com>
2023-12-05 11:56:17 +0100

    calculate shape numpy aray

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+_summary_
+"""
+
+
+def np_shape(matrix):
+    """_summary_
+
+    Args:
+        matrix (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    arr_shape = matrix.shape
+    return arr_shape
