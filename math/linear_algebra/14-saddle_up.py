008934f17832658740436d33f19d82288a0312ba
grem62 <gremontmatheo22@gmail.com>
2023-12-05 12:37:53 +0100

    multiplication of matrice

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+_summary_
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """_summary_
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    multiplication = mat1@mat2
+    return multiplication
