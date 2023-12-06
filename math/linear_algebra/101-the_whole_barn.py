b0ee4a155207cf1d78faaeb67c5f63096bf27360
grem62 <gremontmatheo22@gmail.com>
2023-12-06 15:30:02 +0100

    additional matrix

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
+"""
+    Module to calculate the sum of 2 matrices even 3D
+"""
+
 
 def add_matrices(mat1, mat2):
-    """
-    Adds two matrices element-wise.
-
-    Parameters:
-    - mat1 (list of lists): The first matrix.
-    - mat2 (list of lists): The second matrix.
-
-    Returns:
-    - list of lists: The result of adding the matrices, or None if matrices are not the same shape.
-    """
-    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+    """Main function to calculate the addition
+    of 2 matrices"""
+    if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    new_matrix = []
-    for line in range(len(mat1)):
-        line_matrix = []
-        for column in range (len(mat1[line])):
-            line_matrix.append(mat1[line][column] + mat2[line][column])
-        new_matrix.append(line_matrix)
-    return new_matrix
+    else:
+        return add_recursive(mat1, mat2)
+
+
+def add_recursive(mat1, mat2):
+    """Calculate the addition of 2 matrices
+    using recursion"""
+    if type(mat1[0]) != list:
+        return [mat1[index] + mat2[index] for index in range(len(mat1))]
+    else:
+        result = []
+        for index in range(len(mat1)):
+            inner = add_recursive(mat1[index], mat2[index])
+            result.append(inner)
+        return result
+
+
+def matrix_shape(matrix):
+    """Return the shape of a matrix"""
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    else:
+        return [len(matrix)] + matrix_shape(matrix[0])
