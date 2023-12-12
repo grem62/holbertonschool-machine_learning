62aa99801bdb23a8f4af9066ac1bf42e70c8d89e
grem62 <gremontmatheo22@gmail.com>
2023-12-12 09:59:20 +0100

    sigma sum

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,6 +8,5 @@ def summation_i_squared(n):
     """
     operation squared in range 1-5
     """
-    squared = [i**2 for i in range(1, n + 1)]
-    somme = sum(squared)
+    somme = n * (n + 1) * (2 * n + 1) // 6
     return somme
