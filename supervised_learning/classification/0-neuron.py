6d862b51fbe2f12f42cb633a2bb808369c313936
grem62 <gremontmatheo22@gmail.com>
2024-01-03 11:21:49 +0100

    neuron

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,26 @@
+#!/user/bin/env python3
+import random
+import numpy as np
+
+"""
+    class neuron that defines a single neuron performing binary classification
+"""
+
+
+class Neuron:
+    """_summary_line
+    """
+    def __init__(self, nx):
+        """_summary_
+
+        Args:
+            nx (_type_): _description_
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise TypeError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
