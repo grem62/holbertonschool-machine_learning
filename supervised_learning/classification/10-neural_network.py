7162b551ef75e12784d918531a1a0cc966f1e97f
grem62 <gremontmatheo22@gmail.com>
2024-01-05 13:00:35 +0100

    correction doc

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -92,6 +92,14 @@ class NeuralNetwork:
         return self.__A2
 
     def forward_prop(self, X):
+        """_summary_
+
+        Args:
+            X (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         self.__A1 = 1 / (1 + np.exp(-(np.matmul(self.__W1, X) + self.__b1)))
         self.__A2 = 1 / \
             (1 + np.exp(-(np.matmul(self.__W2, self.__A1) + self.__b2)))
