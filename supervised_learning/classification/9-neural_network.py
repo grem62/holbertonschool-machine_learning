6c7468fb892d8675456b5bef57204a6525c9cf1d
grem62 <gremontmatheo22@gmail.com>
2024-01-05 11:25:08 +0100

    correction file

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -1,96 +1,92 @@
 #!/usr/bin/env python3
-"""_summary_
+"""Neural Network
 """
-
 import numpy as np
-"""_summary_
-
-    Raises:
-        TypeError: _description_
-        ValueError: _description_
-        TypeError: _description_
-        ValueError: _description_
-"""
 
 
 class NeuralNetwork:
     """_summary_
     """
     def __init__(self, nx, nodes):
-        """_summary_
+        """Class contructor
 
         Args:
-            nx (_type_): _description_
-            nodes (_type_): _description_
+            nx (integer): number of input features
+            nodes (integer): number of nodes in the hidden layer
         """
+
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-
+        self.nx = nx
         if type(nodes) is not int:
             raise TypeError('nodes must be an integer')
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
-
-        self.W1 = np.random.normal(size=(nodes, nx))
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-
-        self.W2 = np.random.normal(size=(1, nodes))
-        self.b2 = 0
-        self.A2 = 0
+        self.__W1 = np.random.normal(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
-        """_summary_
+        """Getter function for private attribute W1
 
         Returns:
-            _type_: _description_
+            integer: weights vector for the hidden layer
         """
+
         return self.__W1
 
     @property
     def b1(self):
-        """_summary_
+        """Getter function for private attribute b1
 
         Returns:
-            _type_: _description_
+            integer: bias for the hidden layer
         """
+
         return self.__b1
 
     @property
     def A1(self):
-        """_summary_
+        """Getter function for private attribute A1
 
         Returns:
-            _type_: _description_
+            integer: activated output for the hidden layer
         """
+
         return self.__A1
 
     @property
     def W2(self):
-        """_summary_
+        """Getter function for private attribute W2
 
         Returns:
-            _type_: _description_
+            integer: weights vector for the ouput neuron
         """
+
         return self.__W2
 
     @property
     def b2(self):
-        """_summary_
+        """Getter function for private attribute b2
 
         Returns:
-            _type_: _description_
+            integer: bias for the output neuron
         """
+
         return self.__b2
 
     @property
     def A2(self):
-        """_summary_
+        """Getter function for private attribute A2
 
         Returns:
-            _type_: _description_
+            integer: activated output for the output neuron
         """
+
         return self.__A2
