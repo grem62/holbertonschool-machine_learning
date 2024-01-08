546791a33a1e4245743f6f0802906a63aea181d5
grem62 <gremontmatheo22@gmail.com>
2024-01-08 13:28:50 +0100

    deep neural

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -35,7 +35,7 @@ class DeepNeuralNetwork:
         self.weights = {}
 
         for i in range(1, self.L + 1):
-            if layers[i - 1] < 1 or type(layers[i - 1]) is not int:
+            if layers[i - 1] < 1 or type(layers[i - 1]) is not list:
                 raise TypeError('layers must be a list of positive integers')
             key_W = 'W' + str(i)
             key_b = 'b' + str(i)
