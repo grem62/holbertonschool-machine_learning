f12216053b46b3ba5e707c84d1d936d53ff1f017
grem62 <gremontmatheo22@gmail.com>
2024-01-08 13:16:17 +0100

    deep neural

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """_summary_
+    """
+    def __init__(self, nx, layers):
+        """_summary_
+
+        Args:
+            nx (_type_): _description_
+            layers (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+            TypeError: _description_
+            ValueError: _description_
+        """
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list:
+            raise TypeError('layers must be a list of positive integers')
+        if layers == [] or layers[0] < 1:
+            raise ValueError('layers must be a list of positive integers')
+
+        self.layers = layers
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(1, self.L + 1):
+            key_W = 'W' + str(i)
+            key_b = 'b' + str(i)
+
+            self.weights[key_W] = np.random.randn(layers[i - 1], nx) * \
+                np.sqrt(2 / nx)
+            self.weights[key_b] = np.zeros((layers[i - 1], 1))
+            nx = layers[i - 1]
