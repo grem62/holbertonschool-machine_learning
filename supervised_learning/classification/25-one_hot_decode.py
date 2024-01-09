f0c81760eb8ab777bc8f38b4d6350b6e46062a70
grem62 <gremontmatheo22@gmail.com>
2024-01-09 14:37:39 +0100

    correction code

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -8,6 +8,14 @@ import numpy as np
 
 
 def one_hot_decode(one_hot):
+    """_summary_
+
+    Args:
+        one_hot (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
         return None
     return np.argmax(one_hot, axis=0)
