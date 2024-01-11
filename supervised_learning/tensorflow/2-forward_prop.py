4eee2302176875283cae34f8c1d884d105ddbf4e
grem62 <gremontmatheo22@gmail.com>
2024-01-11 10:20:55 +0100

    forward

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -13,15 +13,14 @@ def forward_prop(x, layer_sizes=[], activations=[]):
 
     Args:
         x (_type_): _description_
-        layer_sizes (list, optional): _description_. Defaults to [].
-        activations (list, optional): _description_. Defaults to [].
+        layer_sizes (_type_): _description_
+        activations (_type_): _description_
 
     Returns:
         _type_: _description_
     """
     for i in range(len(layer_sizes)):
-        if i == 0:
-            pred = create_layer(x, layer_sizes[0], activations[0])
-        else:
-            pred = create_layer(prediction, layer_sizes[i], activations[i])
-        return pred
+        layer = create_layer(x, layer_sizes[i], activations[i])
+        x = layer
+
+    return layer
