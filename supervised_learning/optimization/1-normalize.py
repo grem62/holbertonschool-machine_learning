9254c10307edcfe75ea7cfc6da89ee03f673242f
grem62 <gremontmatheo22@gmail.com>
2024-01-15 15:05:35 +0100

    normalize the matrix

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""_summary_"""
+
+
+import numpy as np
+"""_summary_
+"""
+
+
+def normalize(X, m, s):
+    """_summary_
+
+    Args:
+        X (_type_): _description_
+        m (_type_): _description_
+        s (_type_): _description_
+    """
+    return (X - m / s)
