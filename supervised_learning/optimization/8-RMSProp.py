72d423fd88936918789f6e6cb6ee7bcca734416e
grem62 <gremontmatheo22@gmail.com>
2024-01-16 15:41:03 +0100

    RMSPROP tenserflow

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import tensorflow.compat.v1 as tf
+"""_summary_
+"""
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """_summary_
+
+    Args:
+        loss (_type_): _description_
+        alpha (_type_): _description_
+        beta2 (_type_): _description_
+        epsilon (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return tf.train.RMSPropOptimizer(alpha, decay=beta2, epsilon=epsilon).minimize(loss)
