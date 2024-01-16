e0ddc7ddac347a636ff1a74c02d0dca1d8830e62
grem62 <gremontmatheo22@gmail.com>
2024-01-16 15:22:43 +0100

    RMSPROP

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -3,6 +3,19 @@
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """_summary_
+
+    Args:
+        alpha (_type_): _description_
+        beta2 (_type_): _description_
+        epsilon (_type_): _description_
+        var (_type_): _description_
+        grad (_type_): _description_
+        s (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     new_s = beta2 * s + (1 - beta2) * grad ** 2
     var = var - alpha * grad / (new_s ** (1 / 2) + epsilon)
     return var, new_s
