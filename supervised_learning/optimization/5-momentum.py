97066eb7407c783a61512b9bc3db6ee82c6fe6bb
grem62 <gremontmatheo22@gmail.com>
2024-01-16 13:21:11 +0100

    task momentum gradient

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
+    """_summary_"""
     update = beta1 * v + (1 - beta1) * grad
     new_var = var - alpha * update
     return new_var, update
