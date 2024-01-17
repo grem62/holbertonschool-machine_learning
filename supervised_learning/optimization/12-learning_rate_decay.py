60800c5f721d678fd6ef124e6fc2173aefce8619
grem62 <gremontmatheo22@gmail.com>
2024-01-17 19:11:22 +0100

    learning rate decay tensorflow

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""_summary_"""
+
+import tensorflow.compat.v1 as tf
+"""_summary_"""
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """_summary_"""
+    learning_rate_decay = tf.train.inverse_time_decay(learning_rate=alpha,
+                                                      global_step=global_step,
+                                                      decay_steps=decay_step,
+                                                      decay_rate=decay_rate,
+                                                      staircase=True)
+    return learning_rate_decay
