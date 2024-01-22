43561625d3d837e61dce0b73a3c759fac5c342a8
grem62 <gremontmatheo22@gmail.com>
2024-01-22 15:56:41 +0100

    sensivity

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 """_summary_
+
+    Returns:
+        _type_:
 """
 
+
 import numpy as np
-"""
-import
-"""
 
 
 def sensitivity(confusion):
     """_summary_
 
     Args:
-        confusion (_type_): _description_
+        confusion (_type_): 
     """
-    sensibilité = np.zeros(confusion.shape[0])
+    sensitiv = np.zeros(confusion.shape[0])
     for i in range(confusion.shape[0]):
-        sensibilité[i] = confusion[i][i] / np.sum(confusion[0])
-    return sensibilité
+        sensitiv[i] = confusion[i][i] / np.sum(confusion[0])
+    return sensitiv
