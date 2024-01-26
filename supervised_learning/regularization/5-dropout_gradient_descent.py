7e70a22c8b2ddd484a41086209c2fae69d83206e
grem62 <gremontmatheo22@gmail.com>
2024-01-26 15:56:56 +0100

    correction succesfull

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
+"""_summary_
 """
-This module contains :
-A function that updates the weights of a neural network
-with Dropout regularization using gradient descent
 
-Function:
-   def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
 
-"""
 import numpy as np
 
 
@@ -27,37 +22,26 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
        alpha: is the learning rate
        keep_prob: is the probability that a node will be kept
        L: is the number of layers of the network
-
     """
     N = Y.shape[1]
 
-    c_A_n = 'A{}'.format(L)
-    curr_A = cache[c_A_n]
-
-    dZ_curr = curr_A - Y
+    dZ_curr = cache['A' + str(L)] - Y
 
     for l_n in range(L, 0, -1):
-
-        c_W_n = 'W{}'.format(l_n)
+        c_W_n = 'W' + str(l_n)
         curr_W = weights[c_W_n]
-        p_A_n = 'A{}'.format(l_n - 1)
+        p_A_n = 'A' + str(l_n - 1)
         prev_A = cache[p_A_n]
 
         dW_curr = 1 / N * np.matmul(dZ_curr, prev_A.T)
         dB_curr = 1 / N * np.sum(dZ_curr, axis=1, keepdims=True)
 
-        next_dtanh = 1 - (np.power(prev_A, 2))
+        if l_n > 1:
+            dZ_curr = np.matmul(curr_W.T, dZ_curr)
+            dZ_curr *= cache['D' + str(l_n - 1)] / keep_prob
+            dZ_curr *= 1 - prev_A**2
+        else:
+            dZ_curr = np.matmul(curr_W.T, dZ_curr) * (1 - prev_A**2)
 
-        dA = np.matmul(curr_W.T, dZ_curr) * next_dtanh
-
-        if l_n - 1 > 0:
-            D = cache['D{}'.format(l_n - 1)]
-            dA = dA * D
-            dA = dA / keep_prob
-
-        dZ_curr = np.multiply(dA, next_dtanh)
-
-        c_b_n = 'b{}'.format(l_n)
-
-        weights[c_W_n] = weights[c_W_n] - alpha * dW_curr
-        weights[c_b_n] = weights[c_b_n] - alpha * dB_curr
+        weights[c_W_n] -= alpha * dW_curr
+        weights['b' + str(l_n)] -= alpha * dB_curr
