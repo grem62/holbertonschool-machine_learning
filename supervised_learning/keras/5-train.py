443f86e0a3ca2867761cd524d8f89e949c69c6f8
grem62 <gremontmatheo22@gmail.com>
2024-01-30 12:54:35 +0100

    train with validate data

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""_summary_"""
+
+
+def train_model(network, data,
+                labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """_summary_
+
+    Args:
+        network (_type_): _description_
+        data (_type_): _description_
+        labels (_type_): _description_
+        batch_size (_type_): _description_
+        epochs (_type_): _description_
+        validation_data (_type_, optional): _description_. Defaults to None.
+        verbose (bool, optional): _description_. Defaults to True.
+        shuffle (bool, optional): _description_. Defaults to False.
+    """
+    history = network.fit(data, labels,
+                          batch_size=batch_size,
+                          epochs=epochs, validation_data=validation_data,
+                          verbose=verbose, shuffle=shuffle)
+
+    return history
