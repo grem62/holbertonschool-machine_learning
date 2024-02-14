63da23e4dc5774a534adc1ec34d722d9ef92db7e
grem62 <gremontmatheo22@gmail.com>
2024-02-14 14:11:56 +0100

    try2

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """_summary_
+
+    Returns:
+        _type_: _description_
 """
+
 
 import tensorflow.keras as K
 
@@ -11,31 +15,41 @@ def inception_block(A_prev, filters):
     Args:
         A_prev (_type_): _description_
         filters (_type_): _description_
+
+    Returns:
+        _type_: _description_
     """
     F1, F3R, F3, F5R, F5, FPP = filters
-    chemin1 = K.layers.Conv2D(filters=F3R, kernel_size=(1, 1),
+
+    # 1x1 convolution branch
+    conv1x1 = K.layers.Conv2D(filters=F1, kernel_size=(1, 1),
                               padding='same', activation='relu')(A_prev)
 
-    chemin11 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
-                               padding='same', activation='relu')(chemin1)
+    # 1x1 convolution before 3x3 convolution branch
+    conv3x3_reduce = K.layers.Conv2D(filters=F3R,
+                                     kernel_size=(1, 1),
+                                     padding='same', activation='relu')(A_prev)
+    conv3x3 = K.layers.Conv2D(filters=F3,
+                              kernel_size=(3, 3),
+                              padding='same',
+                              activation='relu')(conv3x3_reduce)
 
-    chemin2 = K.layers.Conv2D(filters=F1, kernel_size=(1, 1),
-                              padding='same', activation='relu')(A_prev)
+    # 1x1 convolution before 5x5 convolution branch
+    conv5x5_reduce = K.layers.Conv2D(filters=F5R,
+                                     kernel_size=(1, 1),
+                                     padding='same', activation='relu')(A_prev)
+    conv5x5 = K.layers.Conv2D(filters=F5,
+                              kernel_size=(5, 5), padding='same',
+                              activation='relu')(conv5x5_reduce)
 
-    chemin3 = K.layers.Conv2D(filters=F5R, kernel_size=(1, 1),
-                              padding='same', activation='relu')(A_prev)
+    # Max pooling branch
+    max_pool = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                     strides=(1, 1), padding='same')(A_prev)
+    conv_pool = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1),
+                                padding='same', activation='relu')(max_pool)
 
-    chemin33 = K.layers.Conv2D(filters='F5', kernel_size=(5, 5),
-                               padding='same', activation='relu')(chemin3)
+    # Concatenate all branches
+    output = K.layers.concatenate([conv1x1, conv3x3, conv5x5, conv_pool],
+                                  axis=3)
 
-    chemin4 = K.layers.MaxPooling2D(filters='filters', kernel_size=(3, 3),
-                                    padding='same', aactivation='relu')(A_prev)
-
-    chemin44 = K.layers.Conv2D(filters='FPP', kernel_size=(1, 1),
-                               padding='same', activation='relu')(A_prev)
-
-    concatenate = K.layers.concatenate([chemin1, chemin11, chemin2,
-                                        chemin3, chemin33, chemin4,
-                                        chemin44], axis='1')
-
-    return concatenate
+    return output
