50e25a2840282d7107aba77687cbaa23d932a96f
grem62 <gremontmatheo22@gmail.com>
2024-02-29 13:07:42 +0100

    cheat the code

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,31 +1,62 @@
 #!/usr/bin/env python3
-"""_summary_
 """
-
+Function that builds an identity block as described in Deep Residual
+Learning for Image Recognition (2015)
+"""
 import tensorflow.keras as K
 
 
 def identity_block(A_prev, filters):
-    """_summary_
-
-    Args:
-        A_prev (_type_): _description_
-        filters (_type_): _description_
-
-    Returns:
-        _type_: _description_
+    """
+    Function that builds an identity block as described in Deep Residual
+    Learning for Image Recognition (2015)
+        Arguments:
+        - A_prev: is the output from the previous layer
+        - filters: is a tuple or list containing F11, F3, F12, respectively:
+            * F11: number of filters in the first 1x1 convolution
+            * F3: number of filters in the 3x3 convolution
+            * F12: number of filters in the second 1x1 convolution
+        Returns: the activated output of the identity block
     """
     F11, F3, F12 = filters
 
-    conv1 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1),
-                            padding='same', activation='relu')(A_prev)
-    conv2 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
-                            padding='same', activation='relu')(conv1)
-    conv3 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
-                            padding='same', activation='relu')(conv2)
-    normalisation = K.layers.normalization.BatchNormalization(epsilon=1e-06, mode=0,
-                                                              momentum=0.9,
-                                                              weights=None)(conv3)
+    # He normal initialization is commonly used for ReLU activations
+    initializer = K.initializers.HeNormal(seed=None)
 
-    output = K.layers.add([normalisation, A_prev])
-    return K.activations.relu(output)
+    # First component of main path
+    conv1 = K.layers.Conv2D(
+        filters=F11,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding='same',
+        kernel_initializer=initializer
+    )(A_prev)
+    batch_normalization_1 = K.layers.BatchNormalization(axis=3)(conv1)
+    relu_1 = K.layers.Activation('relu')(batch_normalization_1)
+
+    # Second component of main path
+    conv2 = K.layers.Conv2D(
+        filters=F3,
+        kernel_size=(3, 3),
+        padding='same',
+        kernel_initializer=initializer
+    )(relu_1)
+    batch_normalization_2 = K.layers.BatchNormalization(axis=3)(conv2)
+    relu_2 = K.layers.Activation('relu')(batch_normalization_2)
+
+    # Third component of main path
+    conv3 = K.layers.Conv2D(
+        filters=F12,
+        kernel_size=(1, 1),
+        padding='same',
+        kernel_initializer=initializer
+    )(relu_2)
+    batch_normalization_3 = K.layers.BatchNormalization(axis=3)(conv3)
+
+    # Add the input to the output
+    sum_result = K.layers.Add()([batch_normalization_3, A_prev])
+
+    # Activate the final output
+    activated_output = K.layers.Activation(activation='relu')(sum_result)
+
+    return activated_output
