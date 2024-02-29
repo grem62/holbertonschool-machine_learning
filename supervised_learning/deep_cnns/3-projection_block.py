1699a5095432d215b8e49b278227042fc455fa2c
grem62 <gremontmatheo22@gmail.com>
2024-02-29 14:23:09 +0100

    correction pycode

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -1,79 +1,79 @@
 #!/usr/bin/env python3
 """
-Function that builds a projection block as described in Deep Residual Learning
+Fonction qui construit un bloc de projection tel que décrit dans Deep Residual Learning
 for Image Recognition (2015)
 """
 import tensorflow.keras as K
 
 
-def projection_block(A_prev, filters, s=2):
+def bloc_projection(A_prev, filtres, s=2):
     """
-    Function that builds a projection block as described in Deep Residual
+    Fonction qui construit un bloc de projection tel que décrit dans Deep Residual
     Learning for Image Recognition (2015)
-        Arguments:
-        - A_prev: the output from the previous layer
-        - filters: a tuple or list containing F11, F3, F12, respectively:
-            * F11: the number of filters in the first 1x1 convolution
-            * F3: the number of filters in the 3x3 convolution
-            * F12: the number of filters in the second 1x1 convolution as well
-            as the 1x1 convolution in the shortcut connection
-        - s: the stride of the first convolution in both the main path and the
-        shortcut connection
-        Returns: the activated output of the projection block
+        Arguments :
+        - A_prev : la sortie de la couche précédente
+        - filtres : un tuple ou une liste contenant F11, F3, F12, respectivement :
+            * F11 : le nombre de filtres dans la première convolution 1x1
+            * F3 : le nombre de filtres dans la convolution 3x3
+            * F12 : le nombre de filtres dans la deuxième convolution 1x1 ainsi que
+            la convolution 1x1 dans la connexion shortcut
+        - s : la taille du pas de la première convolution à la fois dans le chemin principal
+        et la connexion shortcut
+        Retourne : la sortie activée du bloc de projection
     """
-    F11, F3, F12 = filters
+    F11, F3, F12 = filtres
 
-    # He normal initialization is commonly used for ReLU activations
-    initializer = K.initializers.HeNormal(seed=None)
+    # Initialisation He normal est couramment utilisée pour les activations ReLU
+    initialiseur = K.initializers.HeNormal(seed=None)
 
-    # First component of main path
+    # Premier composant du chemin principal
     conv1 = K.layers.Conv2D(
         filters=F11,
         kernel_size=(1, 1),
         strides=s,
         padding='same',
-        kernel_initializer=initializer
+        kernel_initializer=initialiseur
     )(A_prev)
     batch_normalization_1 = K.layers.BatchNormalization(axis=3)(conv1)
     relu_1 = K.layers.Activation('relu')(batch_normalization_1)
 
-    # Second component of main path
+    # Deuxième composant du chemin principal
     conv2 = K.layers.Conv2D(
         filters=F3,
         kernel_size=(3, 3),
         strides=(1, 1),
         padding='same',
-        kernel_initializer=initializer
+        kernel_initializer=initialiseur
     )(relu_1)
     batch_normalization_2 = K.layers.BatchNormalization(axis=3)(conv2)
     relu_2 = K.layers.Activation('relu')(batch_normalization_2)
 
-    # Third component of main path
+    # Troisième composant du chemin principal
     conv3 = K.layers.Conv2D(
         filters=F12,
         kernel_size=(1, 1),
         strides=(1, 1),
         padding='same',
-        kernel_initializer=initializer
+        kernel_initializer=initialiseur
     )(relu_2)
     batch_normalization_3 = K.layers.BatchNormalization(axis=3)(conv3)
 
-    # shortcut connection
+    # Connexion shortcut
     shortcut_connection = K.layers.Conv2D(
         filters=F12,
         kernel_size=(1, 1),
         strides=s,
         padding='same',
-        kernel_initializer=initializer
+        kernel_initializer=initialiseur
     )(A_prev)
     batch_normalization_shortcut = K.layers.BatchNormalization(
         axis=3)(shortcut_connection)
 
-    # Add the shortcut value to the output
+    # Ajouter la valeur shortcut à la sortie
     sum_result = K.layers.Add()(
         [batch_normalization_3, batch_normalization_shortcut])
 
-    # Activate the final output
+    # Activer la sortie finale
     activated_output = K.layers.Activation(activation='relu')(sum_result)
 
     return activated_output
