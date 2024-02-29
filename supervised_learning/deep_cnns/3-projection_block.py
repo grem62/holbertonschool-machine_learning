07f93bd81ec1ce0ec6f21b2d876907d8e1ec7989
grem62 <gremontmatheo22@gmail.com>
2024-02-29 13:24:47 +0100

    correction

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -22,14 +22,20 @@ def projection_block(A_prev, filters, s=2):
     conv1 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1),
                             strides=(s, s), padding='same',
                             activation='relu')(A_prev)
+    normalisation1 = K.layers.BatchNormalization(axis=3)(conv1)
+    relu_1 = K.layers.Activation('relu')(normalisation1)
     conv2 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
-                            padding='same', activation='relu')(conv1)
+                            padding='same', activation='relu')(relu_1)
+    normalisation2 = K.layers.BatchNormalization(axis=3)(conv2)
+    relu2 = K.layers.Activation('relu')(normalisation2)
     conv3 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
-                            padding='same', activation='relu')(conv2)
-
+                            padding='same', activation='relu')(relu2)
+    normalisation3 = K.layers.BatchNormalization(axis=3)(conv3)
+    relu3 = K.layers.Activation('relu')(normalisation3)
     conv4 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
                             strides=(s, s), padding='same',
                             activation='relu')(A_prev)
-
-    output = K.layers.add([conv3, conv4])
+    normalisation4 = K.layers.BatchNormalisation(axis=3)(conv4)
+    relu4 = K.layers.Activation('relu')(normalisation4)
+    output = K.layers.add([normalisation3, normalisation4])
     return K.activations.relu(output)
