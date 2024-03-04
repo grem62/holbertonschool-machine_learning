713a6e3f42b9db1080329d7fa7c3a72e134bbad4
grem62 <gremontmatheo22@gmail.com>
2024-03-04 09:08:55 +0100

    correction pycode

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -22,11 +22,13 @@ def dense_block(X, nb_filters, growth_rate, layers):
         batch_norm1 = K.layers.BatchNormalization()(X)
         activation1 = K.layers.Activation('relu')(batch_norm1)
         conv1 = K.layers.Conv2D(filters=4*growth_rate, kernel_size=1,
-                                padding='same', kernel_initializer=init)(activation1)
+                                padding='same',
+                                kernel_initializer=init)(activation1)
         batch_norm2 = K.layers.BatchNormalization()(conv1)
         activation2 = K.layers.Activation('relu')(batch_norm2)
         conv2 = K.layers.Conv2D(filters=growth_rate, kernel_size=3,
-                                padding='same', kernel_initializer=init)(activation2)
+                                padding='same',
+                                kernel_initializer=init)(activation2)
         X = K.layers.concatenate([X, conv2])
         nb_filters += growth_rate
     return X, nb_filters
