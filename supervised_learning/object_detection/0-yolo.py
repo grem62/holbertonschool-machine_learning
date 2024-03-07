ad647f159720ed26d93f27d0e47e3fc31a1d905f
grem62 <gremontmatheo22@gmail.com>
2024-03-07 13:26:57 +0100

    correction class

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import load_model
 
 
 class Yolo:
-
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """ Yolo class constructor"""
-        self.model = tf.keras.load_model(model_path)
-        with open(classes_path, 'r') as f:
-            self.class_names = [line.strip() for line in f]
+        self.model = load_model(model_path)
+        self.class_names = self._load_classes(classes_path)
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
+
+    def _load_classes(self, classes_path):
+        """
+        Load the class names from a file
+        """
+        with open(classes_path, 'r') as f:
+            class_names = f.readlines()
+        class_names = [name.strip() for name in class_names]
+        return class_names
