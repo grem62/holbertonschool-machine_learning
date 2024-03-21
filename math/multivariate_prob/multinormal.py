23517214978aa513afa3055d1613f3693edf1d47
grem62 <gremontmatheo22@gmail.com>
2024-03-21 16:48:50 +0100

    correction code

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -9,10 +9,11 @@ class MultiNormal:
     """Multinormal class"""
 
     def __init__(self, data):
-        if len(data.shape) != 2:
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
         if data.shape[1] < 2:
-            raise TypeError("data must contain multiple data points")
+            raise ValueError("data must contain multiple data points")
+
         self.mean = np.mean(data, axis=1).reshape(-1, 1)
         self.cov = np.dot((data - self.mean),
                           (data - self.mean).T) / (data.shape[1] - 1)
