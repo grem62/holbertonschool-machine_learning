0e22228c4b23ba08154f027e70893899b8d2eeab
grem62 <gremontmatheo22@gmail.com>
2024-03-25 11:02:45 +0100

    correction type error

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -21,7 +21,7 @@ def likelihood(x, n, P):
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be an integer that is greater than 0")
+        raise ValueError("n must be a positive integer")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
