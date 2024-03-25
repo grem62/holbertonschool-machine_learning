a989de3c0246188d7bd667f9e18114285fe09bda
grem62 <gremontmatheo22@gmail.com>
2024-03-25 11:06:05 +0100

    correction type error

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -54,13 +54,15 @@ def intersection(x, n, P, Pr):
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be an integer that is greater than 0")
+        raise ValueError("n must be a positive integer")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if not np.isclose(np.sum(Pr), 1):
