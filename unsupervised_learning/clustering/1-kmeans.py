483db26f9d599b09b1b806ab6ae36a6b0c771dd6
grem62 <gremontmatheo22@gmail.com>
2024-04-04 16:33:09 +0200

    correction

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -18,7 +18,7 @@ def kmeans(X, k, iterations=1000):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    
+
     # Détermination du nombre de lignes et de colonnes de la matrice X
     n, d = X.shape
 
@@ -50,14 +50,15 @@ def kmeans(X, k, iterations=1000):
             if len(X[clss == j]) == 0:
                 C[j] = np.random.uniform(np.min(X, axis=0),
                                          np.max(X, axis=0), (1, d))
-        # Sinon, le centre est mis à jour en prenant la moyenne des points du cluster
+        # Sinon, le centre est mis à jour en prenant
+        # la moyenne des points du cluster
             else:
                 C[j] = np.mean(X[clss == j], axis=0)
 
         # Vérification de la convergence du K-means
         if np.array_equal(C, C_copy):
             return C, clss
-    
+
     D = np.linalg.norm(X - C[:, np.newaxis], axis=2)
     clss = np.argmin(D, axis=0)
     return C, clss
