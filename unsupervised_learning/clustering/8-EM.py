f541db2c4e4666873bf2821384f25cd6264383f8
grem62 <gremontmatheo22@gmail.com>
2024-04-07 22:42:22 +0200

    m

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env python3
 """ Expectation Maximization for Gaussian Mixture Model """
-
 import numpy as np
-initialize = __import__('4-initialize').initialize
-expectation = __import__('6-expectation').expectation
-maximization = __import__('7-maximization').maximization
+from initialize import initialize
+from expectation import expectation
+from maximization import maximization
 
 
-def expectation_maximization(X, num_clusters,
-                             num_iterations=1000,
+def expectation_maximization(X,
+                             num_clusters,
+                             iterations=1000,
                              tolerance=1e-5,
                              verbose=False):
     """
-    Performs the Expectation Maximization for a Gaussian Mixture Model
+    Performs Expectation Maximization for Gaussian Mixture Model
     Args:
-        - X: np.ndarray (n, d) data set
+        - X: np.ndarray (n, d) - data set
             - n: number of data points
             - d: number of dimensions
-        - num_clusters: positive int, number of clusters
-        - num_iterations: positive int, number of iterations
-        - tolerance: non-negative float, tolerance of log likelihood
-        - verbose: bool for printing information
+        - num_clusters: positive int
+        - number of clusters
+        - iterations: positive int - number of iterations
+        - tolerance: non-negative float - tolerance of log likelihood
+        - verbose: bool - print information
     Returns:
-        - pi: np.ndarray (num_clusters,) of priors for each clust
-        - means: np.ndarray (num_clusters, d) of centroid means for each clust
-        - covariances: np.ndarray (num_clusters, d, d) of
-        - covariance matrices for each cluster
-        - probabilities: np.ndarray (num_clusters, n) of
+        - pi: np.ndarray (num_clusters,) - priors for each cluster
+        - means: np.ndarray (num_clusters, d) - centroid means for each cluster
+        - covariances: np.ndarray (num_clusters, d, d)
+        -covariance matrices for each cluster
+        - probabilities: np.ndarray (num_clusters, n)
         - probabilities for each data point in each cluster
-        - log_likelihood: log likelihood of the model
+        - log_likelihood: float - log likelihood of the model
     """
     # Input Validation
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
     if not isinstance(num_clusters, int) or num_clusters <= 0:
         return None, None, None, None, None
-    if not isinstance(num_iterations, int) or num_iterations <= 0:
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None, None
     if not isinstance(tolerance, float) or tolerance <= 0:
         return None, None, None, None, None
@@ -50,14 +51,16 @@ def expectation_maximization(X, num_clusters,
     prev_log_likelihood = 0
 
     # EM iterations
-    for iteration in range(num_iterations):
-        # Verbose mode: printing log likelihood after every 10 iterations
-        if verbose and iteration % 10 == 0:
+    for i in range(iterations):
+        # Verbose mode: print log likelihood after every 10 iterations
+        if verbose and i % 10 == 0:
             print('Log Likelihood after {} iterations: {}'.format(
-                iteration, log_likelihood.round(5)))
+                i, log_likelihood.round(5)))
 
-        # Maximization and Expectation steps
+        # Maximization step
         priors, means, covariances = maximization(X, probabilities)
+
+        # Expectation step
         probabilities, log_likelihood = expectation(
             X, priors, means, covariances)
 
@@ -71,6 +74,6 @@ def expectation_maximization(X, num_clusters,
     # Final log likelihood
     if verbose:
         print('Log Likelihood after {} iterations: {}'.format(
-            iteration+1, log_likelihood.round(5)))
+            i+1, log_likelihood.round(5)))
 
     return priors, means, covariances, probabilities, log_likelihood
