519c28ccc9c753a62b15fd0f6ed15ff96dc4afba
grem62 <gremontmatheo22@gmail.com>
2024-04-07 21:49:42 +0200

    add task kmean with import sklearn

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+
+
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """Performs K-means on a dataset."""
+    kmeans = sklearn.cluster.KMeans(n_clusters=k)
+    kmeans.fit(X)
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+    return C, clss
