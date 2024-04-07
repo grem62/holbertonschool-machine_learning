d3d5c72f3436f177d82545d276713c48e7f39f2a
grem62 <gremontmatheo22@gmail.com>
2024-04-07 21:56:21 +0200

    correction comment

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
 
 import numpy as np
 
 
 def maximization(X, g):
-    # X: Input data matrix of shape (n, d),
-    # where n is the number of samples and d is the number of features
-    # g: Matrix of shape (k, n) representing the
-    # posterior probabilities of each sample belonging to each cluster
+    # X: Matrice de données d'entrée de forme (n, d),
+    # où n est le nombre d'échantillons et d est le nombre de caractéristiques
+    # g: Matrice de forme (k, n) représentant les
+    # probabilités a posteriori de chaque échantillon appartenant à chaque cluster
     n, d = X.shape
     k = g.shape[0]
 
-    # Calculate updated mixture coefficients (pi) values
+    # Calculer les valeurs mises à jour des coefficients de mélange (pi)
     pi = np.sum(g, axis=1) / n
 
-    # Initialize arrays to store updated means (m) and covariances (S)
+    # Initialiser les tableaux pour stocker les moyennes (m) et les covariances (S) mises à jour
     m = np.zeros((k, d))
     S = np.zeros((k, d, d))
 
-    # Update means and covariances for each cluster
+    # Mettre à jour les moyennes et les covariances pour chaque cluster
     for i in range(k):
-        # Calculate updated mean for cluster i
+        # Calculer la moyenne mise à jour pour le cluster i
         m[i] = np.dot(g[i], X) / np.sum(g[i])
 
-        # Calculate the difference between each sample
-        # and the mean of cluster i
+        # Calculer la différence entre chaque échantillon
+        # et la moyenne du cluster i
         diff = X - m[i]
 
-        # Calculate updated covariance matrix for cluster i
+        # Calculer la matrice de covariance mise à jour pour le cluster i
         S[i] = np.dot(g[i] * diff.T, diff) / np.sum(g[i])
 
-    # Return the updated mixture coefficients, means, and covariances
+    # Retourner les coefficients de mélange, les moyennes et les covariances mises à jour
     return pi, m, S
