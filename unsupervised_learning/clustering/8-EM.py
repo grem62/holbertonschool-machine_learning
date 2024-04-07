fd68ed925988ddfaa4462bd0a0198440911d2488
grem62 <gremontmatheo22@gmail.com>
2024-04-07 22:12:44 +0200

    correction if

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Expectation Maximization for Gaussian Mixture Model """
 
+import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
