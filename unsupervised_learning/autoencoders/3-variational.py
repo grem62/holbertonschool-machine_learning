7f44128edb4b7d7584c4abba091f20df817e9a49
grem62 <gremontmatheo22@gmail.com>
2024-04-24 16:50:36 +0200

    variational

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -7,37 +7,42 @@ from tensorflow.keras import layers
 from keras import backend as K
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
-    inputs = K.Input(shape=(input_dims,))
-    h = layers.Dense(hidden_layers, activation='relu')(inputs)
-    z_mean = layers.Dense(latent_dims)(h)
-    z_log_sigma = layers.Dense(latent_dims)(h)
+    """creates a variational autoencoder"""
+    # Encoder
+    input_encoder = K.Input(shape=(input_dims,))
+    output_encoder = input_encoder
+    for units in hidden_layers:
+        output_encoder = K.layers.Dense(units, activation='relu')(output_encoder)
+    z_mean = K.layers.Dense(latent_dims)(output_encoder)
+    z_log_sigma = K.layers.Dense(latent_dims)(output_encoder)
 
     def sampling(args):
         z_mean, z_log_sigma = args
-        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dims),
-                              mean=0., stddev=0.1)
-        return z_mean + K.exp(z_log_sigma) * epsilon
+        batch = K.shape(z_mean)[0]
+        epsilon = K.random_normal(shape=(batch, latent_dims), mean=0., stddev=1.)
+        return z_mean + K.exp(z_log_sigma / 2) * epsilon
 
-    z = layers.Lambda(sampling)([z_mean, z_log_sigma])
+    z = K.layers.Lambda(sampling)([z_mean, z_log_sigma])
 
-    encoder = K.Model(inputs, [z_mean, z_log_sigma, z], name='encoder')
+    encoder = K.Model(input_encoder, [z, z_mean, z_log_sigma])
 
-    # Create decoder
-    latent_inputs = K.Input(shape=(latent_dims,), name='z_sampling')
-    x = layers.Dense(hidden_layers, activation='relu')(latent_inputs)
-    outputs = layers.Dense(input_dims, activation='sigmoid')(x)
-    decoder = K.Model(latent_inputs, outputs, name='decoder')
+    # Decoder
+    input_decoder = K.Input(shape=(latent_dims,))
+    output_decoder = input_decoder
+    for units in reversed(hidden_layers):
+        output_decoder = K.layers.Dense(units, activation='relu')(output_decoder)
+    output_decoder = K.layers.Dense(input_dims, activation='sigmoid')(output_decoder)
+    decoder = K.Model(input_decoder, output_decoder)
 
-    # instantiate VAE model
-    outputs = decoder(encoder(inputs)[2])
-    vae = K.Model(inputs, outputs, name='vae_mlp')
+    # Autoencoder
+    output_autoencoder = decoder(encoder(input_encoder)[0])
+    autoencoder = K.Model(input_encoder, output_autoencoder)
 
-    reconstruction_loss = K.losses.binary_crossentropy(inputs, outputs)
-    reconstruction_loss *= input_dims
-    kl_loss = 1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    vae_loss = K.mean(reconstruction_loss + kl_loss)
-    vae.add_loss(vae_loss)
-    vae.compile(optimizer='adam')
-    return encoder, decoder, vae
+    def vae_loss(x, x_decoded_mean):
+        xent_loss = K.losses.binary_crossentropy(x, x_decoded_mean)
+        kl_loss = - 0.5 * K.sum(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
+        return K.mean(xent_loss + kl_loss)
+
+    autoencoder.compile(optimizer='adam', loss=vae_loss)
+
+    return encoder, decoder, autoencoder
