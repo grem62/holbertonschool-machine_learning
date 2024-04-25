59112fd0de25335dd9ae918798da776cfebeb5b6
grem62 <gremontmatheo22@gmail.com>
2024-04-25 12:50:27 +0200

    convolution

---
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import tensorflow.keras as K
+
+
+def autoencoder(input_dims, filters, latent_dims):
+    # Encoder
+    input_img = K.Input(shape=input_dims)
+    encoded = input_img
+    for num_filters in filters:
+        encoded = K.layers.Conv2D(num_filters,
+                                  (3, 3), activation='relu',
+                                  padding='same')(encoded)
+        encoded = K.layers.MaxPooling2D((2, 2),
+                                        padding='same')(encoded)
+
+    # Latent space
+    latent = encoded
+
+    # Decoder
+    for num_filters in reversed(filters[:-1]):
+        decoded = K.layers.Conv2D(num_filters, (3, 3),
+                                  activation='relu',
+                                  padding='same')(latent)
+        decoded = K.layers.UpSampling2D((2, 2))(decoded)
+
+    decoded = K.layers.Conv2D(filters[-1], (3, 3),
+                              activation='relu',
+                              padding='valid')(decoded)
+    decoded = K.layers.UpSampling2D((2, 2))(decoded)
+    decoded = K.layers.Conv2D(input_dims[-1], (3, 3),
+                              activation='sigmoid',
+                              padding='same')(decoded)
+
+    # Models
+    encoder = K.Model(input_img, latent)
+    decoder = K.Model(latent, decoded)
+    auto = K.Model(input_img, decoder(encoder(input_img)))
+
+    # Compile
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
