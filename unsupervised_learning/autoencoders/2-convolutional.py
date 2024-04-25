4e060b4c4edf2b398fb2096e1f8b6f27a4a9c9f0
grem62 <gremontmatheo22@gmail.com>
2024-04-25 13:14:41 +0200

    correction neurone network

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -39,10 +39,10 @@ def autoencoder(input_dims, filters, latent_dims):
                               activation='relu',
                               padding='same')(dec)
         dec = K.layers.UpSampling2D((2, 2))(dec)
-    dec = K.layers.Conv2D(filters[-1], (3, 3),
+    dec = K.layers.Conv2D(-1, (3, 3),
                           activation='relu', padding='valid')(dec)
     dec = K.layers.UpSampling2D((2, 2))(dec)
-    dec = K.layers.Conv2D(filters[-1], (3, 3),
+    dec = K.layers.Conv2D(1, (3, 3),
                           activation='sigmoid', padding='same')(dec)
     decoder = K.models.Model(decoder_input, dec)
 
