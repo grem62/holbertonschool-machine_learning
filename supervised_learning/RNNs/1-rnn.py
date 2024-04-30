d8067800f48057d22c62b941393489c14ad11008
grem62 <gremontmatheo22@gmail.com>
2024-04-30 14:17:23 +0200

    perform forward

---
+++ supervised_learning/RNNs/1-rnn.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """
+    Effectue une propagation avant pour un réseau de neurones récurrents.
+
+    Args:
+        rnn_cell (RNNCell): Cellule RNN utilisée.
+        X (ndarray): Tableau de forme (t, m, i) contenant les données
+                     d'entrée.
+            t (int): Nombre d'instants dans la séquence.
+            m (int): Nombre d'éléments dans le lot.
+            i (int): Dimension de l'entrée.
+        h_0 (ndarray): Tableau de forme (m, h) contenant l'état initial.
+
+    Returns:
+        ndarray: Tableau de forme (t, m, h) contenant les états cachés pour
+                 chaque instant dans la séquence.
+        ndarray: Tableau de forme (t, m, o) contenant les sorties pour chaque
+                 instant dans la séquence.
+    """
+    t, m, i = X.shape
+    m, h = h_0.shape
+    o = rnn_cell.Wy.shape[1]
+    H = np.zeros((t + 1, m, h))
+    Y = np.zeros((t, m, o))
+    H[0] = h_0
+    for timestep in range(t):
+        H[timestep + 1], Y[timestep] = rnn_cell.forward(H[timestep],
+                                                        X[timestep])
+    return H[1:], Y
