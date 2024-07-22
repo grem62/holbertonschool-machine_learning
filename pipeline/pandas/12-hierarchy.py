5ffd66c529c2e8f16e2d94bdc6e721e9c00c8f77
grem62 <gremontmatheo22@gmail.com>
2024-07-22 16:49:34 +0200

    hierarchy

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+
+df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
+
+df1 = df1.set_index('Timestamp')
+df2 = df2.set_index('Timestamp')
+
+df1_filtered = df1.loc[1417411980:1417417980]
+df2_filtered = df2.loc[1417411980:1417417980]
+
+combined_df = pd.concat([df2_filtered, df1_filtered], keys=['bitstamp', 'coinbase'])
+
+combined_df = combined_df.swaplevel(0, 1)
+combined_df.sort_index(inplace=True)
+print(combined_df)
