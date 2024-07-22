384a60a9978722e0884911869feb5e71b5289b22
grem62 <gremontmatheo22@gmail.com>
2024-07-22 11:34:53 +0200

    Data frame

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+import pandas as pd
+
+data = {
+    'First': ['0.0', '0.5', '1.0', '1.5'],
+    'Second': ["one", "two", "three", "four"],
+
+}
+
+index_labels = ['A', 'B', 'C', 'D']
+
+df = pd.DataFrame(data, index=index_labels)
