b7e4a29e4422f4100514713c791fbc513c688b34
grem62 <gremontmatheo22@gmail.com>
2024-07-23 14:27:20 +0200

    pycode

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -3,24 +3,27 @@
 
 import requests
 
+
 def rocket_frequency():
     """_summary_
     """
     launches_url = "https://api.spacexdata.com/v4/launches"
     rockets_url = "https://api.spacexdata.com/v4/rockets"
     launches = requests.get(launches_url).json()
-    rockets = {rocket["id"]: rocket["name"] for rocket in requests.get(rockets_url).json()}
+    rockets = {rocket["id"]: rocket["name"] for rocket in requests.get(
+        rockets_url).json()}
     launch_counts = {}
 
     for launch in launches:
         rocket_name = rockets.get(launch["rocket"], "Unknown")
         launch_counts[rocket_name] = launch_counts.get(rocket_name, 0) + 1
 
-    sorted_launch_counts = sorted(launch_counts.items(), key=lambda item: item[1], reverse=True)
+    sorted_launch_counts = sorted(launch_counts.items(),
+                                  key=lambda item: item[1], reverse=True)
 
     for rocket_name, count in sorted_launch_counts:
         print("{}: {}".format(rocket_name, count))
 
+
 if __name__ == '__main__':
     rocket_frequency()
-
