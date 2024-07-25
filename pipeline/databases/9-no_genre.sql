13fd7d5facc22b589c5363f7b0fe0de7f9eaa146
grem62 <gremontmatheo22@gmail.com>
2024-07-25 13:30:18 +0200

    correction output

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,7 +1,7 @@
 -- Find the TV shows without a genre
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows AS tv_shows
-LEFT JOIN hbtn_0d_tv_show_genres AS tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
