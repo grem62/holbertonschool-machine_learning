7ce933ee5244e1de735c04ba1ba8e5c13f9d646d
grem62 <gremontmatheo22@gmail.com>
2024-07-25 13:24:10 +0200

    correction output

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
--- Number of shows by genre
-SELECT genre, COUNT(*) AS number_of_shows
-FROM hbtn_0d_tvshows
-GROUP BY genre
-HAVING COUNT(*) > 0
-ORDER BY number_of_shows DESC;
+-- comment
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre ORDER BY number_of_shows DESC;
