1cf08d4fad6c668e9dbc3718a6ed7d47e5b9889d
grem62 <gremontmatheo22@gmail.com>
2024-07-29 10:13:52 +0200

    add all file

---
+++ pipeline/databases/15-fans.sql
@@ -0,0 +1,5 @@
+-- comment
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,7 @@
+-- comment
+CREATE TRIGGER UpdateItems
+AFTER INSERT ON orders
+FOR EACH ROW
+	UPDATE items
+	SET items.quantity = items.quantity - NEW.number
+	WHERE items.name = NEW.item_name;
