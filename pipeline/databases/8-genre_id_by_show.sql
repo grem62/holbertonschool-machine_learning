375d656bf61411884016fc8fc4406f8cfdd60932
grem62 <gremontmatheo22@gmail.com>
2024-07-29 10:08:26 +0200

    add file

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- comment
+-- This query returns the title of each TV show and the genre_id of each genre associated 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
